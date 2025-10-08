c14e9926f99178067a87e69a88842334433e64fe
Austin Kerbow <Austin.Kerbow@amd.com>
2025-10-07 17:06:56 -0700

    Reapply "AMDGPU: Move reg_sequence splat handling (#140313)"
    This reverts commit c8d27ce221ab8af8021d51616de5213011dcd638.

--- llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.cpp
+++ llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.cpp
@@ -1346,7 +1346,7 @@ unsigned getNumSGPRBlocks(const MCSubtargetInfo *STI, unsigned NumSGPRs) {
 
 unsigned getVGPRReductionToIncreaseWavesPerEU(const MCSubtargetInfo *STI,
                                               unsigned NumVGPRs) {
-  unsigned Granule = getVGPRAllocGranule(STI);
+  unsigned Granule = getVGPRAllocGranule(STI, 0, std::nullopt);
   unsigned MaxWaves = getMaxWavesPerEU(STI);
   unsigned TotalNumVGPRs = getTotalNumVGPRs(STI);
 
